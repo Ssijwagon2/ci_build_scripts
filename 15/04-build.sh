01abf66ce915c210655d88b2f47bb62fa3210158
AidanWarner97 <aidan@awarner.co.uk>
2026-03-26 20:10:15 +0000

    04-build.sh: Add cleanup logic for Google devices

--- 15/04-build.sh
+++ 15/04-build.sh
@@ -26,4 +26,28 @@ if [[ "$buildformat" == "Installclean" ]]; then
     m installclean
 fi
 
+google_devices="caiman comet tokay komodo tegu shiba husky akita tangorpro felix lynx panther cheetah bluejay oriole raven sargo bonito blueline crosshatch"
+
+if [[ " $google_devices " == *" $device "* ]]; then
+    SOURCE_PATH="vendor/gms/product/packages/privileged_apps/"
+    TARGET_ROOT="vendor/google"
+    TARGET_SUBPATH="proprietary/product/priv-app"
+
+    for source_dir in "$SOURCE_PATH"/*; do
+        base_name=$(basename "$source_dir")
+
+        target_dirs=$(find "$TARGET_ROOT" -type d -path "*/$TARGET_SUBPATH/$base_name")
+
+        if [[ -n "$target_dirs" ]]; then
+            echo "Match found for $base_name in $TARGET_ROOT. Removing $source_dir..."
+            rm -rf "$source_dir"
+        else
+            echo "No match found for $base_name. Keeping $source_dir."
+        fi
+    done
+
+else
+    echo "Device $device is not in the list of Google devices, no need for cleanup."
+fi
+
 m evolution -j$(nproc --all)
