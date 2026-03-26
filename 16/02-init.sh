932e0603b8c1a6caf8725f060eca2e4bb0b8f257
AidanWarner97 <aidan@awarner.co.uk>
2026-03-26 21:07:30 +0100

    [TEMP] Update branch to bq2 for QPR2 source

--- 16/02-init.sh
+++ 16/02-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth=1 --git-lfs
+repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --prune
 
 mkdir -p /home/shared/11-source/vendor/evolution-priv/
 rsync -av /home/shared/vendor/evolution-priv/keys/ /home/shared/11-source/vendor/evolution-priv/keys/
